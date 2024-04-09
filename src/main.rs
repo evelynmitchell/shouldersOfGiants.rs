cfa757cecaa760e7313f8637d550e14d0b24d6cb
evelynmitchell <efmphone@gmail.com>
2024-04-08 19:22:43 -0600

    adding the model struct

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ struct GPT2Config {
     channels: usize,    //number of channels in fully connected layers, e.g. 768
 }
 
+struct GPT2Model {
+    config: GPT2Config,
+    // other fields
+}
+
 fn main() {
     // Create a new GPT2Config object
     let config = GPT2Config {
@@ -16,10 +21,23 @@ fn main() {
         channels: 768,
     };
 
+    let model = GPT2Model {
+        config: config,
+        // other fields
+    };
+
     // Access the fields of the config object
     println!("Max sequence length: {}", config.max_seq_len);
     println!("Vocab size: {}", config.vocab_size);
     println!("Number of layers: {}", config.num_layers);
     println!("Number of heads: {}", config.num_heads);
     println!("Number of channels: {}", config.channels);
+
+    // Access the fields of the model object
+    println!("Model config - max sequence length: {}", model.config.max_seq_len);
+    println!("Model config - vocab size: {}", model.config.vocab_size);
+    println!("Model config - number of layers: {}", model.config.num_layers);
+    println!("Model config - number of heads: {}", model.config.num_heads);
+    println!("Model config - number of channels: {}", model.config.channels);
+    
 }
